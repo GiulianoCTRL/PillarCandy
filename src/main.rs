b066b9a947b613d24a3308e7d661136536f6a486
GiulianoCTRL <35188493+GiulianoCTRL@users.noreply.github.com>
2021-03-17 23:00:41 +0100

    Quick commit to fix build issue
    The purpose of this commit is mainly to fix the build issues on macOS (M1).
    
    Additionally, some experimentation has been done with scrollables and the
    select crate.
    
    Pushed to main to be able to continue working on this on my main device.

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,29 @@
 //! Application used to find passages and related laws from riksdagen.se
 
 use iced::{
-    button, text_input, Align, Button, Column, Element, Length, Row, Sandbox, Settings, Text,
-    TextInput,
+    button, text_input, scrollable, Align, Button, Column, Element, Length, Row, Sandbox, Settings, Text,
+    TextInput, Scrollable, Space
 };
 
 use pillar_candy::Law;
 
+use select;
+use select::document::Document;
+use select::predicate::{Name, Predicate};
+
+/// This is the app itself, currently it contains the states for the search
+/// button and search input, as well as information about the search text itself
+/// and the result from the law query.
 #[derive(Default)]
 struct PillarCandy {
     search_confirmed: button::State,
     search_input: text_input::State,
+    result_state: scrollable::State,
     search_text: String,
     result_text: String,
 }
 
+/// Message enum notifying the app if the search changes or if the search has been confirmed
 #[derive(Debug, Clone)]
 enum Message {
     SearchChanged(String),
@@ -32,17 +41,30 @@ impl Sandbox for PillarCandy {
         String::from("PillarCandy")
     }
 
+    /// When the search text changes the search text will be updated,
+    /// a request will only be sent once the search button has been pressed.
     fn update(&mut self, message: Message) {
         match message {
             Message::SearchChanged(s) => {
                 self.search_text = s;
             }
             Message::ConfirmSearch => {
-                self.result_text = Law::from_string(&self.search_text).text();
+                // Bottom is just playing around, will be moved and refactored into lib.rs
+                // Works for now but non existend tags need to be handled
+                let html: &str = &Law::from_string(&self.search_text).text();
+                let document = Document::from(html);
+                let node = document.select(Name("dokumentstatus").descendant(Name("dokument"))).next().unwrap();
+                let invalid = "NA";
+                let id = node.select(Name("beteckning")).next().expect(invalid).text();
+                let date = node.select(Name("datum")).next().expect(invalid).text();
+                let titel = node.select(Name("titel")).next().expect(invalid).text();
+                let text = node.select(Name("text")).next().expect(invalid).text();
+                self.result_text = format!("[{}] {} ({})\n\n{}", id, titel, date, text);
             }
         }
     }
 
+    /// The GUI layout
     fn view(&mut self) -> Element<Message> {
         let search_field = TextInput::new(
             &mut self.search_input,
@@ -59,10 +81,12 @@ impl Sandbox for PillarCandy {
             .push(search_field)
             .push(search_button.on_press(Message::ConfirmSearch));
 
-        // TODO: Scrollable seems to be extremly laggy, but Text doesn't display the full text.
-        let result = Text::new(&self.result_text)
+        // TODO: Scrollable seems to be extremly laggy, but Text doesn't display the full text. Workaround?
+        let result = Scrollable::new(&mut self.result_state)
+            .padding(10)
             .width(Length::Fill)
-            .height(Length::Fill);
+            .height(Length::Fill)
+            .push(Text::new(&self.result_text));
 
         Column::new()
             .padding(20)
